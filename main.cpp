2ed81c0ee4da459f550e3f15da7c4dd7972842d5
itou-syunnya-0826 <k023g1015@g.neec.ac.jp>
2023-10-15 01:43:10 +0900

    弾とりあえず出してみた

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <Novice.h>
+#include <stdlib.h>
+#include <time.h>
 
 const char kWindowTitle[] = "GC1D_03_イトウシュンヤ";
 
@@ -18,11 +20,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	}Vecter2;
 
 	typedef struct Player {
-		Vector2 position;
-		Vector2 velocity;//速度
-		Vector2 acceleration;//加速度
-		float scale;
-		float speed;
+		Vector2 position;//X,100 Y,0
+		Vector2 velocity;//速度0
+		Vector2 acceleration;//加速度-0.8
+		float scale;//1
+		float speed;//20
 	}Player;
 	
 	Player player{
@@ -33,11 +35,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		20.0f
 	};
 
-	bool IsPlayer = false;
+	int JampSystem = 0;
+	int IsJump = 0;
+	int IsJampTimer = 0;
+	int JampTimer = 25;//ジャンプのラグ
 	float newposY = 0;
-	float JumpNum = 0;
-	float CoolDown = 180;
-	float HipDrop = 0;
 
 	
 
@@ -89,7 +91,51 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int monitorX = worldPosX - scrollX;//ワールド座標とスクロール値を引いた値をモニターから見た自分の座標に代入する
 
 
+	//弾
 
+	typedef struct Bullet1 {
+		Vector2 position;
+		float speed;
+		float scale;
+	}Bullet1;
+	
+	typedef struct Bullet2 {
+		Vector2 position;
+		float speed;
+		float scale;
+	}Bullet2;
+
+	typedef struct Bullet3 {
+		Vector2 position;
+		float speed;
+		float scale;
+	}Bullet3;
+
+	Bullet1 bullet1{
+		{0.0f,0.0f},
+		10.0f,
+		1.0f
+	};
+
+	Bullet2 bullet2{
+		{0.0f,0.0f},
+		10.0f,
+		1.0f
+	};
+
+	Bullet3 bullet3{
+		{0.0f,0.0f},
+		10.0f,
+		1.0f
+	};
+
+	bool IsBullet1 = false;
+	bool IsBullet2 = false;
+	bool IsBullet3 = false;
+
+	int tama1 = Novice::LoadTexture("./tama.png");
+	int tama2 = Novice::LoadTexture("./tama.png");
+	int tama3 = Novice::LoadTexture("./tama.png");
 
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -131,23 +177,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 
 		};
-
-
-
-
+		
+		
+		//position
+		newposY = (player.position.y - 480) * -1;//ここでplayerのY座標を決める
+		//Boss
+		NewBossPosY = (boss.position.y - 415) * -1;
 
 
 		//背景のスクロール
 
 		worldPosX += int(Speed);//ワールド座標を右方向に動かす
 		scrollX += int(Speed);//スクロール値も更新する
-
-		boss.position.x -= int(boss.speed);
-
-		//BOSSの登場処理
-		if (boss.position.x == 1150) {
-			boss.speed = 0;
-		}
 
 		if (worldPosX > 7623) {
 			scrollX -= int(Speed);
@@ -163,66 +204,102 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			Speed = 0;
 		}
 
+		//BOSSの登場処理
+		if (boss.position.x == 1150) {
+			boss.speed = 0;
+		}
+
+		boss.position.x -= int(boss.speed);
+
+
+		
+
 
 
 		//playerの挙動
 
 		//二段ジャンプ
-
-		if (JumpNum == 0 || JumpNum == 1) {
-			if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
-				IsPlayer = true;
-				player.velocity.y = 19.0;
-				JumpNum += 1;
-				
+		if (IsJump == 0 || IsJump == 1) {//ジャンプ０回と１回の時に
+			if (IsJampTimer == 0) {//ジャンプラグ
+				if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {//ジャンプ押したら
+					JampSystem = 1;//ジャンプ
+					player.velocity.y = 19.0;
+					IsJump += 1;
+					IsJampTimer = 1;//ジャンプのラグ計算開始
+				}
 			}
 		}
 
-		
-		
-
-		if (JumpNum == 2) {
-			CoolDown--;
+		//ジャンプのラグ
+		if (IsJampTimer == 1) {
+			JampTimer--;//25フレーム
 		}
 
-		if (CoolDown == 179) {
-			HipDrop += 1;
+		if (JampTimer == 0) {//ラグタイマーが終わったら
+			IsJampTimer = 0;//ラグタイマー変数を0に戻す
+			JampTimer = 25;//ラグタイマーを25フレームに戻す
 		}
 
-		if (CoolDown == 0) {
-			JumpNum = 0;
-			CoolDown = 180;
-			HipDrop = 0;
+		//2回目のジャンプ
+		if (IsJump == 2) {
+			if (IsJampTimer == 0) {
+				if (keys[DIK_SPACE]) {//長押しでヒップドロップ
+					player.position.y -= player.speed;
+				}
+			}
+
 		}
 
-		if (IsPlayer == true) {
+
+		//ジャンプしたら
+		if (JampSystem == 1) {//ジャンプの仕組み
 			player.velocity.y += player.acceleration.y;
 			player.position.y += player.velocity.y;
 		}
 
+		//謎---ジャンプして元の位置に戻すためのだった
 		if (player.position.y < player.scale) {
 			player.position.y = player.scale;
+			IsJump = 0;//元の位置に戻ったらジャンプ０
 		}
 
-		//長押し
-		if (keys[DIK_SPACE]&&HipDrop==1) {
-			player.position.y -= player.speed;
+		//弾の発射
+
+		if (keys[DIK_UP]&&!preKeys[DIK_UP]) {
+			IsBullet1 = true;
+			IsBullet2 = true;
+			IsBullet3 = true;
+			bullet1.position.x = 1280;
+			bullet2.position.x = 1280;
+			bullet3.position.x = 1280;
+			bullet1.position.y = 500;
+			bullet2.position.y = 300;
+			bullet3.position.y = 100;
 		}
 
+		if (IsBullet1 == true) {
+			bullet1.position.x -= bullet1.speed;
+		}
 
+		if (IsBullet2 == true) {
+			bullet2.position.x -= bullet2.speed;
+		}
 
+		if (IsBullet3 == true) {
+			bullet3.position.x -= bullet3.speed;
+		}
 
+		if (bullet1.position.x == -10) {
+			IsBullet1 = false;
+		}
 
+		if (bullet2.position.x == -10) {
+			IsBullet2 = false;
+		}
 
-		newposY = (player.position.y - 480) * -1;//ここでplayerのY座標を決める
-
-
-
-
-		//Boss
-
-		NewBossPosY = (boss.position.y - 415) * -1;
-
+		if (bullet3.position.x == -10) {
+			IsBullet3 = false;
+		}
 
 
 		///
@@ -234,11 +311,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		
 		//値確認
-		Novice::ScreenPrintf(100, 100, "JumpNum=%f", JumpNum);
-		Novice::ScreenPrintf(100,130,"Cooldown=%f",CoolDown);
-		Novice::ScreenPrintf(100,160,"HipDrop=%f",HipDrop);
-		
-
+		Novice::ScreenPrintf(100, 100, "isJump=%d", IsJump);
+		Novice::ScreenPrintf(100, 130, "isjampTimer=%d", IsJampTimer);
+		Novice::ScreenPrintf(100, 160, "jampTimer=%d", JampTimer);
+		Novice::ScreenPrintf(100, 190, "IsBullet=%d", IsBullet1);
 
 		//背景
 		Novice::DrawSprite(0 - scrollX, 0, BackGround[0], 1.0f, 1.0f, 0.0f, WHITE);
@@ -247,11 +323,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Novice::DrawSprite(3840 - scrollX, 0, BackGround[3], 1.0f, 1.0f, 0.0f, WHITE);
 		Novice::DrawSprite(5120 - scrollX, 0, BackGround[4], 1.0f, 1.0f, 0.0f, WHITE);
 		Novice::DrawSprite(6400 - scrollX, 0, BackGround[5], 1.0f, 1.0f, 0.0f, WHITE);
-
-
-
-
-
 
 		//マップチップ描画
 		
@@ -262,25 +333,27 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				}
 			}
 		}
-		
-		
-
-
-
-
-
-		
-		//player描画
-		//Novice::DrawSprite((int)player.position.x, (int)newposY, sample, player.scale, player.scale, 0.0f, WHITE);
+		//playerの描画
 		
 		Novice::DrawSprite((int)monitorX - 512, (int)newposY, sample, player.scale, player.scale, 0.0f, WHITE);//32*32
 
+		//Bossの描画
 		Novice::DrawSprite((int)boss.position.x, (int)NewBossPosY, BossHandle, 1.0f, 1.0f, 0.0f, boss.color);
 
 		
+		//弾の描画
 
+		if (IsBullet1 == true) {
+			Novice::DrawSprite((int)bullet1.position.x, (int)bullet1.position.y, tama1, bullet1.scale, bullet1.scale, 0.0f, WHITE);
+		}
 
+		if (IsBullet2 == true) {
+			Novice::DrawSprite((int)bullet2.position.x, (int)bullet2.position.y, tama2, bullet2.scale, bullet2.scale, 0.0f, WHITE);
+		}
 
+		if (IsBullet3 == true) {
+			Novice::DrawSprite((int)bullet3.position.x, (int)bullet3.position.y, tama3, bullet3.scale, bullet3.scale, 0.0f, WHITE);
+		}
 
 		///
 		/// ↑描画処理ここまで
