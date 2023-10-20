c5981f66244cef4143f9ea3c73222013cee78c2d
arima-naoto <k023g1010@g.neec.ac.jp>
2023-10-20 21:12:26 +0900

    シーン切り替えの追加

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <Novice.h>
 #include <stdlib.h>
+#include <math.h>
 
 const char kWindowTitle[] = "5142";
 
@@ -13,6 +14,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};	
 
+	enum OneButton {
+		TITLE,
+		STAGE1,
+		BOSS,
+		GAMECLEAR,
+		GAMEOVER,
+	};
+
 	//Vector2構造体の宣言
 	typedef struct Vector2 {
 		float x;
@@ -24,8 +33,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Vector2 position;//X,100 Y,0
 		Vector2 velocity;//速度0
 		Vector2 acceleration;//加速度-0.8
+		float radius;
 		float scale;//1
 		float speed;//20
+		float distance;
 	}Player;
 
 	//Player構造体の初期化
@@ -33,8 +44,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		{100.0f,0.0f},
 		{0.0f,0.0f},
 		{0.0f,-0.8f},
+		25.0f,
 		1.0f,
-		20.0f
+		20.0f,
+		0.0f
 	};
 	int sample = Novice::LoadTexture("./sample.png");//プレイヤーの描画
 
@@ -62,7 +75,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	for (int i = 0; i < Max; i++) {
 		bullet[i].position.x = 0.0f;
 		bullet[i].position.y = -10.0f;
-		bullet[i].radius = 10.0f;
+		bullet[i].radius = 30.0f;
 		bullet[i].speed = 7.0f;
 		bullet[i].color = WHITE;
 		bullet[i].isShot = false;
@@ -71,14 +84,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//Boss構造体の宣言==================================================================================
 	typedef struct Boss {
 		Vector2 position;
-		float speed;
 		unsigned int color;
 	}Boss;
 
 	//Boss構造体の初期化================================================================================
 	Boss boss{
-		{7550.0f,0.0f},
-		5.0f,
+		{1150.0f,0.0f},
 		WHITE
 	};
 
@@ -134,6 +145,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int BulletCoolTimer = 15;
 	//スクロール時の弾のクールタイムを15に設定する
 
+	int Scene = 0;
+	//シーン切り替え用の変数Sceneを宣言し,0で初期化する
+
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -147,170 +161,238 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		//playerの挙動
-		
-		//背景のスクロール
+		switch (Scene) {
 
-		worldPosX += ScrollSpeedX;//ワールド座標を右方向に動かす
-		scrollX += ScrollSpeedX;//スクロール値も更新する
+		case TITLE:
 
-		boss.position.x -= boss.speed;
-		//BOSSのX座標をスピード5で左に動かす
-
-		//BOSSの登場処理
-		if (boss.position.x == 1150) {//ボスのX座標を1150まで到達したら
-			boss.speed = 0;//ボスのスピードを0にする
-		}
-
-
-		//ボスの複数弾処理==============================================================================
-		if (boss.position.x == 1150) {
-			if (BossBulletCoolTimer > 0) {
-				BossBulletCoolTimer--;
+			if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == false) {
+				Scene = STAGE1;
 			}
-			else {
-				BossBulletCoolTimer = 15;
+			break;
+
+		case STAGE1:
+
+			//playerの挙動
+
+			//背景のスクロール
+
+			worldPosX += ScrollSpeedX;//ワールド座標を右方向に動かす
+			scrollX += ScrollSpeedX;//スクロール値も更新する
+
+			//背景のスクロール処理==========================================================================
+			if (worldPosX > 7623) {
+				scrollX -= ScrollSpeedX;
 			}
 
-			if (BossBulletCoolTimer <= 0) {
-				for (int i = 0; i < Max2; i++) {
-					if (bossBullet[i].isShot == false) {
-						bossBullet[i].isShot = true;
+			if (worldPosX > 7040 || worldPosX > 7680) {//ワールド座標が3200かつ3840の場合
+				scrollX = 6400;//スクロール値を6400で固定する
+			}
 
-						bossBullet[i].position.x = boss.position.x - 15.0f;
-						bossBullet[i].position.y = boss.position.y + 468.0f + rand() % 400 - 350;
+			monitorX = worldPosX - scrollX;
+
+			if (scrollX == 6400) {//スクロール値が6400まで到達したら
+				ScrollSpeedX = 0;//スクロールのスピードを0にする
+				Scene = BOSS;
+			}
+
+			//スクロール時の複数弾の処理====================================================================
+			if (worldPosX <= 7040) {
+				if (BulletCoolTimer > 0) {
+					BulletCoolTimer--;
+				}
+				else {
+					BulletCoolTimer = 15;
+				}
+
+				if (BulletCoolTimer <= 0) {
+					for (int i = 0; i < Max; i++) {
+						if (bullet[i].isShot == false) {
+							bullet[i].isShot = true;
+
+							bullet[i].position.x = 1270.0f;
+							bullet[i].position.y = 500.0f + rand() % 400 - 350;
+							break;
+						}
+					}
+				}
+			}
+
+			for (int i = 0; i < Max; i++) {//for文処理
+				if (bullet[i].isShot == true) {//bullet[i].isShotがtrueだったら
+					bullet[i].position.x -= bullet[i].speed;
+					//スクロール時の弾を左方向に動かす
+
+					if (bullet[i].position.x <= 0) {//スクロール時の弾のX座標が0まで到達したら
+						bullet[i].isShot = false;//スクロール時の弾のフラグをfalseにする
 						break;
 					}
 				}
 			}
-		}
 
-
-		for (int i = 0; i < Max2; i++) {
-			if (bossBullet[i].isShot == true) {
-				bossBullet[i].position.x -= bossBullet[i].speed;
-
-				if (bossBullet[i].position.x <= -54) {
-					bossBullet[i].isShot = false;
-					break;
-				}
-			}
-		}
-
-		//背景のスクロール処理==========================================================================
-		if (worldPosX > 7623) {
-			scrollX -= ScrollSpeedX;
-		}
-
-		if (worldPosX > 7040 || worldPosX > 7680) {//ワールド座標が3200かつ3840の場合
-			scrollX = 6400;//スクロール値を6400で固定する
-		}
-
-		monitorX = worldPosX - scrollX;
-
-		if (scrollX == 6400) {//スクロール値が6400まで到達したら
-			ScrollSpeedX = 0;//スクロールのスピードを0にする
-		}
-
-		//スクロール時の複数弾の処理====================================================================
-		if (worldPosX <= 7040) {
-			if (BulletCoolTimer > 0) {
-				BulletCoolTimer--;
-			}
-			else {
-				BulletCoolTimer = 15;
-			}
-
-			if (BulletCoolTimer <= 0) {
+			if (worldPosX >= 7040) {//WorldPosXが7040以上だったら
 				for (int i = 0; i < Max; i++) {
-					if (bullet[i].isShot == false) {
-						bullet[i].isShot = true;
+					bullet[i].isShot = false;
+					//スクロール時の弾のフラグをfalseに設定する
+				}
+			}
 
-						bullet[i].position.x = 1270.0f;
-						bullet[i].position.y = 500.0f + rand() % 400 - 350;
-						break;
+			//===================================<プレイヤーのジャンプ処理>=================================
+
+			//二段ジャンプ
+			if (IsJump == 0 || IsJump == 1) {//ジャンプ０回と１回の時に
+				if (isjampTimer == 0) {//ジャンプラグ
+					if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {//ジャンプ押したら
+						Jampsystem = 1;//ジャンプ
+						player.velocity.y = 19.0;
+						IsJump += 1;
+						isjampTimer = 1;//ジャンプのラグ計算開始
 					}
 				}
 			}
-		}
 
-		for (int i = 0; i < Max; i++) {//for文処理
-			if (bullet[i].isShot == true) {//bullet[i].isShotがtrueだったら
-				bullet[i].position.x -= bullet[i].speed;
-				//スクロール時の弾を左方向に動かす
+			//ジャンプのラグ
+			if (isjampTimer == 1) {
+				jampTimer--;//25フレーム
+			}
 
-				if (bullet[i].position.x <= 0) {//スクロール時の弾のX座標が0まで到達したら
-					bullet[i].isShot = false;//スクロール時の弾のフラグをfalseにする
-					break;
+			if (jampTimer == 0) {//ラグタイマーが終わったら
+				isjampTimer = 0;//ラグタイマー変数を0に戻す
+				jampTimer = 25;//ラグタイマーを25フレームに戻す
+			}
+
+			//2回目のジャンプ
+			if (IsJump == 2) {
+				if (isjampTimer == 0) {
+					if (keys[DIK_SPACE]) {//長押しでヒップドロップ
+						player.position.y -= player.speed;
+					}
 				}
+
 			}
-		}
 
-		if (worldPosX >= 7040) {//WorldPosXが7040以上だったら
-			for (int i = 0; i < Max; i++) {
-				bullet[i].isShot = false;
-				//スクロール時の弾のフラグをfalseに設定する
+			//ジャンプしたら
+			if (Jampsystem == 1) {//ジャンプの仕組み
+				player.velocity.y += player.acceleration.y;
+				player.position.y += player.velocity.y;
 			}
-		}
 
-		//===================================<プレイヤーのジャンプ処理>=================================
+			//謎---ジャンプして元の位置に戻すためのだった
+			if (player.position.y < player.scale) {
+				player.position.y = player.scale;
+				IsJump = 0;//元の位置に戻ったらジャンプ０
+			}
+			newposY = (player.position.y - 480) * -1;//ここでplayerのY座標を決める
 
-		//二段ジャンプ
-		if (IsJump == 0 || IsJump == 1) {//ジャンプ０回と１回の時に
-			if (isjampTimer == 0) {//ジャンプラグ
-				if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {//ジャンプ押したら
-					Jampsystem = 1;//ジャンプ
-					player.velocity.y = 19.0;
-					IsJump += 1;
-					isjampTimer = 1;//ジャンプのラグ計算開始
+			//値確認
+			Novice::ScreenPrintf(100, 100, "isJump=%d", IsJump);
+			Novice::ScreenPrintf(100, 130, "isjampTimer=%d", isjampTimer);
+			Novice::ScreenPrintf(100, 160, "jampTimer=%d", jampTimer);
+
+
+			Novice::ScreenPrintf(100, 190, "worldPosX = %d", worldPosX);
+			Novice::ScreenPrintf(100, 220, "scrollX = %d", scrollX);
+			break;
+
+			case BOSS:
+
+				//===================================<プレイヤーのジャンプ処理>=================================
+
+			//二段ジャンプ
+				if (IsJump == 0 || IsJump == 1) {//ジャンプ０回と１回の時に
+					if (isjampTimer == 0) {//ジャンプラグ
+						if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {//ジャンプ押したら
+							Jampsystem = 1;//ジャンプ
+							player.velocity.y = 19.0;
+							IsJump += 1;
+							isjampTimer = 1;//ジャンプのラグ計算開始
+						}
+					}
 				}
-			}
-		}
 
-		//ジャンプのラグ
-		if (isjampTimer == 1) {
-			jampTimer--;//25フレーム
-		}
-
-		if (jampTimer == 0) {//ラグタイマーが終わったら
-			isjampTimer = 0;//ラグタイマー変数を0に戻す
-			jampTimer = 25;//ラグタイマーを25フレームに戻す
-		}
-
-		//2回目のジャンプ
-		if (IsJump == 2) {
-			if (isjampTimer == 0) {
-				if (keys[DIK_SPACE]) {//長押しでヒップドロップ
-					player.position.y -= player.speed;
+				//ジャンプのラグ
+				if (isjampTimer == 1) {
+					jampTimer--;//25フレーム
 				}
-			}
+
+				if (jampTimer == 0) {//ラグタイマーが終わったら
+					isjampTimer = 0;//ラグタイマー変数を0に戻す
+					jampTimer = 25;//ラグタイマーを25フレームに戻す
+				}
+
+				//2回目のジャンプ
+				if (IsJump == 2) {
+					if (isjampTimer == 0) {
+						if (keys[DIK_SPACE]) {//長押しでヒップドロップ
+							player.position.y -= player.speed;
+						}
+					}
+
+				}
+
+				//ジャンプしたら
+				if (Jampsystem == 1) {//ジャンプの仕組み
+					player.velocity.y += player.acceleration.y;
+					player.position.y += player.velocity.y;
+				}
+
+				//謎---ジャンプして元の位置に戻すためのだった
+				if (player.position.y < player.scale) {
+					player.position.y = player.scale;
+					IsJump = 0;//元の位置に戻ったらジャンプ０
+				}
+
+				//ボスの複数弾処理==============================================================================
+				if (boss.position.x == 1150) {
+					if (BossBulletCoolTimer > 0) {
+						BossBulletCoolTimer--;
+					}
+					else {
+						BossBulletCoolTimer = 15;
+					}
+
+					if (BossBulletCoolTimer <= 0) {
+						for (int i = 0; i < Max2; i++) {
+							if (bossBullet[i].isShot == false) {
+								bossBullet[i].isShot = true;
+
+								bossBullet[i].position.x = boss.position.x - 15.0f;
+								bossBullet[i].position.y = boss.position.y + 468.0f + rand() % 400 - 350;
+								break;
+							}
+						}
+					}
+				}
+
+
+				for (int i = 0; i < Max2; i++) {
+					if (bossBullet[i].isShot == true) {
+						bossBullet[i].position.x -= bossBullet[i].speed;
+
+						if (bossBullet[i].position.x <= -54) {
+							bossBullet[i].isShot = false;
+							break;
+						}
+					}
+				}
+
+				newposY = (player.position.y - 480) * -1;//ここでplayerのY座標を決める
+				NewBossPosY = (boss.position.y - 415) * -1;//ここでplayerのY座標を決める
+
+				break;
+
+			case GAMECLEAR:
+				if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == false) {
+					Scene = GAMEOVER;
+				}
+				break;
+
+			case GAMEOVER:
+				if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == false) {
+					Scene = TITLE;
+				}
+				break;
 
 		}
-
-
-		//ジャンプしたら
-		if (Jampsystem == 1) {//ジャンプの仕組み
-			player.velocity.y += player.acceleration.y;
-			player.position.y += player.velocity.y;
-		}
-
-		//謎---ジャンプして元の位置に戻すためのだった
-		if (player.position.y < player.scale) {
-			player.position.y = player.scale;
-			IsJump = 0;//元の位置に戻ったらジャンプ０
-		}
-
-		newposY = (player.position.y - 480) * -1;//ここでplayerのY座標を決める
-		NewBossPosY = (boss.position.y - 415) * -1;//ここでplayerのY座標を決める
-
-		//値確認
-		Novice::ScreenPrintf(100, 100, "isJump=%d", IsJump);
-		Novice::ScreenPrintf(100, 130, "isjampTimer=%d", isjampTimer);
-		Novice::ScreenPrintf(100, 160, "jampTimer=%d", jampTimer);
-
-		
-		Novice::ScreenPrintf(100, 190, "worldPosX = %d", worldPosX);
-		Novice::ScreenPrintf(100, 220, "monitorX = %d", monitorX);
 
 		///
 		/// ↑更新処理ここまで
@@ -320,34 +402,61 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		Novice::DrawSprite(0 - scrollX,  0, BackGround[0], 1.0f, 1.0f, 0.0f, WHITE);
-		Novice::DrawSprite(1280 - scrollX, 0, BackGround[1], 1.0f, 1.0f, 0.0f, WHITE);
-		Novice::DrawSprite(2560 - scrollX, 0, BackGround[2], 1.0f, 1.0f, 0.0f, WHITE);
-		Novice::DrawSprite(3840 - scrollX, 0, BackGround[3], 1.0f, 1.0f, 0.0f, WHITE);
-		Novice::DrawSprite(5120 - scrollX, 0, BackGround[4], 1.0f, 1.0f, 0.0f, WHITE);
-		Novice::DrawSprite(6400 - scrollX, 0, BackGround[5], 1.0f, 1.0f, 0.0f, WHITE);
+		switch (Scene) {
 
-		Novice::DrawSprite((int)monitorX - 512, (int)newposY, sample, player.scale, player.scale, 0.0f, WHITE);
+		case TITLE:
+			Novice::DrawBox(0, 0, 1280, 640, 0.0f, BLUE, kFillModeSolid);
 
-		Novice::DrawSprite((int)boss.position.x, (int)NewBossPosY, BossHandle, 1.0f, 1.0f, 0.0f, boss.color);
+			break;
 
-		for (int i = 0; i < Max; i++) {
-			if (bullet[i].isShot == true) {
-				Novice::DrawEllipse(int(bullet[i].position.x), int(bullet[i].position.y),
-					                int(bullet[i].radius), int(bullet[i].radius), 
-					                0.0f, bullet[i].color, kFillModeSolid);
-				//for文を使用し、bullet[i].isShotがTrueだった場合、DrawSpriteで画像を描画する
+		case STAGE1:
+			Novice::DrawSprite(0 - scrollX, 0, BackGround[0], 1.0f, 1.0f, 0.0f, WHITE);
+			Novice::DrawSprite(1280 - scrollX, 0, BackGround[1], 1.0f, 1.0f, 0.0f, WHITE);
+			Novice::DrawSprite(2560 - scrollX, 0, BackGround[2], 1.0f, 1.0f, 0.0f, WHITE);
+			Novice::DrawSprite(3840 - scrollX, 0, BackGround[3], 1.0f, 1.0f, 0.0f, WHITE);
+			Novice::DrawSprite(5120 - scrollX, 0, BackGround[4], 1.0f, 1.0f, 0.0f, WHITE);
+			Novice::DrawSprite(6400 - scrollX, 0, BackGround[5], 1.0f, 1.0f, 0.0f, WHITE);
+
+			Novice::DrawSprite((int)player.position.x - (int)player.radius, (int)newposY - (int)player.radius, sample, player.scale, player.scale, 0.0f, WHITE);
+
+			for (int i = 0; i < Max; i++) {
+				if (bullet[i].isShot == true) {
+					Novice::DrawEllipse(int(bullet[i].position.x), int(bullet[i].position.y),
+						int(bullet[i].radius) / 3, int(bullet[i].radius) / 3,
+						0.0f, bullet[i].color, kFillModeSolid);
+					//for文を使用し、bullet[i].isShotがTrueだった場合、DrawSpriteで画像を描画する
+				}
 			}
-		}
 
-		for (int i = 0; i < Max2; i++) {
-			if (bossBullet[i].isShot == true) {
-				Novice::DrawSprite((int)bossBullet[i].position.x, (int)bossBullet[i].position.y, 
-					                BossBulletHandle,1,1,0.0f, (int)bossBullet[i].color);
-				//for文を使用し、bossBullet[i].isShotがTrueだった場合、DrawSpriteで画像を描画する
+			break;
+
+		case BOSS:
+
+			Novice::DrawSprite(6400 - scrollX, 0, BackGround[5], 1.0f, 1.0f, 0.0f, WHITE);
+
+			Novice::DrawSprite((int)player.position.x - (int)player.radius, (int)newposY - (int)player.radius, sample, player.scale, player.scale, 0.0f, WHITE);
+
+			Novice::DrawSprite((int)boss.position.x, (int)NewBossPosY, BossHandle, 1.0f, 1.0f, 0.0f, boss.color);
+
+			for (int i = 0; i < Max2; i++) {
+				if (bossBullet[i].isShot == true) {
+					Novice::DrawSprite((int)bossBullet[i].position.x, (int)bossBullet[i].position.y,
+						BossBulletHandle, 1, 1, 0.0f, (int)bossBullet[i].color);
+					//for文を使用し、bossBullet[i].isShotがTrueだった場合、DrawSpriteで画像を描画する
+				}
 			}
-		}
 
+			break;
+
+		case GAMECLEAR:
+			Novice::DrawBox(0, 0, 1280, 640, 0.0f, RED, kFillModeSolid);
+			break;
+
+		case GAMEOVER:
+			Novice::DrawBox(0, 0, 1280, 640, 0.0f, BLACK, kFillModeSolid);
+			break;
+
+		}
 		///
 		/// ↑描画処理ここまで
 		///
